30978a0dfb49fbbbd8c8a75ba3f612d1a7c44f15
reconnecting <a2446787178@gmail.com>
2019-06-22 10:42:47 +0800

    first use unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,20 @@
 from selenium import webdriver
-broswer = webdriver.Chrome()
+import unittest
 
-broswer.get('http://localhost:8000')
-assert "Django" in broswer.title
+class NewVistorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(10)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('TO-DO',self.browser.title)
+        self.fail('finish the test')
+
+if __name__ =='__main__':
+    # unittest.main(warnings='ignore')
+    unittest.main()
+
