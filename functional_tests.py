bb30f9379ffb0847469f8eb7504124a1ec94aa77
reconnecting <a2446787178@gmail.com>
2019-07-07 12:05:33 +0800

    第一次完整的测试

--- functional_tests.py
+++ functional_tests.py
@@ -11,7 +11,7 @@ class NewVistorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('TO-DO',self.browser.title)
+        self.assertIn('To-Do',self.browser.title)
         self.fail('finish the test')
 
 if __name__ =='__main__':
