84a42a88f45957570f85164af55280c04c6a5ea3
reconnecting <a2446787178@gmail.com>
2019-07-07 15:59:43 +0800

    增加功能测试的范围

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
+
 
 class NewVistorTest(unittest.TestCase):
     def setUp(self):
@@ -10,11 +12,31 @@ class NewVistorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # 打开浏览器
         self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do',self.browser.title)
+
+        # 断言to-do在title、h1标签里面
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # 校验输入框的默认文字
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # 输入框输入文字：。。。。。
+        inputbox.send_keys('Buy peacock feathers')
+
+        #
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(rows.text == '1: Buy peacock feathers' for row in rows))
+
         self.fail('finish the test')
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
     # unittest.main(warnings='ignore')
     unittest.main()
-
